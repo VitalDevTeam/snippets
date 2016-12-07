9f4a3c4a447bc9dd73f1b6c2cd46f578a324f7b7
Adam Walter <adam@adamwalter.com>
2016-12-07 10:53:28 -0500

    Standard Gravity Form template embed

---
+++ wordpress/gravity-forms/gform-template-embed.php
@@ -0,0 +1,3 @@
+<?php
+// https://www.gravityhelp.com/documentation/article/embedding-a-form/
+gravity_form( $id_or_title, $display_title = true, $display_description = true, $display_inactive = false, $field_values = null, $ajax = false, $tabindex, $echo = true );
