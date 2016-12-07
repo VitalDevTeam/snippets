b53d6ddeead45f2495c62954ec80c5131b89d6c0
Adam Walter <adam@adamwalter.com>
2016-12-07 12:38:51 -0500

    Generates a unique user ID (Microsoft-style)

---
+++ js/generate-uuid.js
@@ -0,0 +1,10 @@
+// Generates a unique user ID (Microsoft-style)
+function generateUUID() {
+    var d = Date.now();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+       var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c == 'x' ? r : (r&0x7|0x8)).toString(16);
+    });
+    return uuid;
+}
