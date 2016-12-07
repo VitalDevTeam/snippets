33cad743d8c284eef21cdcac94d54362c5e37f82
Adam Walter <adam@adamwalter.com>
2016-12-07 11:14:56 -0500

    MagnificPopup Catch-all

---
+++ js/magnific-popup-catchall.js
@@ -0,0 +1,98 @@
+// Magnific Popup Catch-all
+
+var mpYouTubeOpts = {
+    type: 'iframe',
+    iframe: {
+        patterns: {
+            youtube: {
+                index: 'youtube.com/',
+                id: 'v=',
+                src: 'https://www.youtube.com/embed/%id%?autoplay=1&autohide=1&modestbranding=1&rel=0&showinfo=0'
+            },
+            youtube_short: {
+                index: 'youtu.be/',
+                id: 'youtu.be/',
+                src: 'https://www.youtube.com/embed/%id%?autoplay=1&autohide=1&modestbranding=1&rel=0&showinfo=0'
+            }
+        }
+    },
+    removalDelay: 300,
+    mainClass: 'mfp-fade'
+},
+mpVimeoOpts = {
+    type: 'iframe',
+    removalDelay: 300,
+    mainClass: 'mfp-fade'
+},
+mpImageOpts = {
+    type: 'image',
+    closeOnContentClick: true,
+    fixedContentPos: true,
+    image: {
+        verticalFit: true,
+        titleSrc: function(item) {
+            var link    = item.el,
+                img     = link.find('img'),
+                title   = img.attr('title'),
+                alt     = img.attr('alt'),
+                caption = '';
+            if (typeof alt !== 'undefined') {
+                caption = alt;
+            } else if (typeof title !== 'undefined') {
+                caption = title;
+            }
+            return caption;
+        }
+    },
+    zoom: {
+        enabled: true,
+        duration: 300,
+        opener: function(element) {
+            return element.find('img');
+        }
+    }
+},
+mpGalleryOpts = {
+    type: 'image',
+    delegate: 'a',
+    image: {
+        verticalFit: true,
+        titleSrc: function(item) {
+            var link    = item.el,
+                img     = link.find('img'),
+                title   = img.attr('title'),
+                alt     = img.attr('alt'),
+                caption = '';
+            if (typeof alt !== 'undefined') {
+                caption = alt;
+            } else if (typeof title !== 'undefined') {
+                caption = title;
+            }
+            return caption;
+        }
+    },
+    gallery: {
+        enabled: true
+    },
+    zoom: {
+        enabled: true,
+        duration: 300,
+        opener: function(element) {
+            return element.find('img');
+        }
+    }
+};
+
+// Links with .lightbox class
+$('.lightbox').magnificPopup();
+
+// Images (images linked to media file)
+$('a[href$=".jpg"], a[href$=".png"], a[href$=".jpeg"], a[href$=".gif"]').not('.gallery a').magnificPopup(mpImageOpts);
+
+// Image galleries (images linked to media file)
+$('.gallery').magnificPopup(mpGalleryOpts);
+
+// Video links
+$('a[href*="youtube.com"]').magnificPopup(mpYouTubeOpts);
+$('a[href*="youtu.be"]').magnificPopup(mpYouTubeOpts);
+$('a[href*="vimeo.com"]').magnificPopup(mpVimeoOpts);
