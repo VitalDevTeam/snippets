f4d22a6c472aa90ac2a31fa36734d48a95dcbb6b
Adam Walter <adam@adamwalter.com>
2016-12-08 17:02:14 -0500

    wp_nav_menu caching

---
+++ wordpress/wp-nav-menu-caching.php
@@ -0,0 +1,100 @@
+<?php
+
+/** --------------------------------
+FOR SIMPLE MENUS WITH NO ACTIVE NAV ITEMS
+------------------------------------ */
+
+// In your template:
+$menu_query = get_transient('main_menu_query');
+
+if ($menu_query === false ) {
+
+    $menu_query = wp_nav_menu(array(
+        'theme_location' => 'main_nav',
+        'echo' => 0,
+    ));
+
+    set_transient('main_menu_query', $menu_query, 1 * HOUR_IN_SECONDS);
+
+}
+
+echo $menu_query;
+
+// In functions.php:
+
+/**
+ * Delete menu cache when menus or posts are updated
+ */
+function delete_menu_transients() {
+    delete_transient('main_menu_query');
+}
+
+add_action('wp_update_nav_menu', 'delete_menu_transients');
+add_action('save_post', 'delete_menu_transients');
+
+
+/** --------------------------------------------
+FOR MENUS WITH ACTIVE NAV ITEMS
+You must generate a unique transient for each page
+------------------------------------------------ */
+
+// Setup unique key
+$key = md5($_SERVER['REQUEST_URI']);
+
+// Create unique transient
+$menu_query = get_transient('main_menu_' . $key);
+
+if ($menu_query === false) {
+
+    $menu_query = wp_nav_menu(array(
+        'theme_location' => 'main_nav',
+        'echo' 			 => 0,
+    ));
+
+    // No expiration for best results
+    set_transient('main_menu_' . $key, $menu_query);
+}
+
+echo $menu_query;
+
+// In functions.php:
+
+function delete_unique_menu_transients() {
+
+    global $wpdb;
+
+    // The main menu transient prefix
+    $prefix = esc_sql('main_menu_');
+
+    $options = $wpdb->options;
+
+    $t  = esc_sql("_transient_$prefix%");
+
+    $sql = $wpdb->prepare(
+        "
+          SELECT option_name
+          FROM $options
+          WHERE option_name LIKE '%s'
+        ",
+        $t
+    );
+
+    $transients = $wpdb->get_col($sql);
+
+    // For each transient...
+    foreach($transients as $transient) {
+
+        // Strip away the WordPress prefix in order to arrive at the transient key.
+        $key = str_replace('_transient_', '', $transient);
+
+        // Now that we have the key, use WordPress core to the delete the transient.
+        delete_transient($key);
+    }
+
+    // Also flush the object cache
+    wp_cache_flush();
+
+}
+
+add_action('wp_update_nav_menu', 'delete_unique_menu_transients');
+add_action('save_post', 'delete_unique_menu_transients');
