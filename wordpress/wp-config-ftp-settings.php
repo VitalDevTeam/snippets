ecd1f8c679930596e47ff4bd990be82c6fd54251
Adam Walter <adam@adamwalter.com>
2016-12-08 16:26:38 -0500

    FTP config for wp-config.php

---
+++ wordpress/wp-config-ftp-settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * FTP Settings
+ */
+//define('FS_METHOD', 'ftpext');
+//define('FTP_BASE', '/path/to/wordpress/');
+//define('FTP_CONTENT_DIR', '/path/to/wordpress/wp-content/');
+//define('FTP_PLUGIN_DIR ', '/path/to/wordpress/wp-content/plugins/');
+//define('FTP_PUBKEY', '/home/username/.ssh/id_rsa.pub');
+//define('FTP_PRIKEY', '/home/username/.ssh/id_rsa');
+define('FTP_USER', 'username');
+define('FTP_PASS', 'password');
+define('FTP_HOST', 'ftp.example.com');
+define('FTP_SSL', false);
