af6c5444b820625094d17a9dea2e6d206d3bc080
Adam Walter <adam@adamwalter.com>
2016-12-08 16:27:58 -0500

    Test if current post is an ancestor of given post ID

---
+++ wordpress/is-tree.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Test if current post is an ancestor of given post ID
+ * @param  integer $pid Post ID of parent
+ * @return boolean
+ */
+function is_tree($pid) {
+    global $post;
+    if (is_page($pid))
+        return true;
+    $anc = get_post_ancestors($post->ID);
+    foreach ($anc as $ancestor) {
+        if (is_page() && $ancestor == $pid) {
+            return true;
+        }
+    }
+    return false;
+}
