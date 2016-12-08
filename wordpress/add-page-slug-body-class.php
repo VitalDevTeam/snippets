2ee10c4125d8af1fb5b022dff2aaf934e3b7d345
Adam Walter <adam@adamwalter.com>
2016-12-08 10:25:22 -0500

    Add new class to body based on page slug

---
+++ wordpress/add-page-slug-body-class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Add new class to body based on page slug
+ */
+function add_body_class($classes) {
+    global $post;
+    if (isset($post)) {
+        $classes[] = $post->post_type . '-' . $post->post_name;
+    }
+    return $classes;
+}
+add_filter('body_class', 'add_body_class');
