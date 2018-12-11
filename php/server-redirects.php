519f1c0015e5c9c89f3f068a77c0c27e842fe2b4
Adam Walter <adam@vtldesign.com>
2018-12-11 13:04:03 -0500

    Updates server-redirects.php

--- php/server-redirects.php
+++ php/server-redirects.php
@@ -7,24 +7,38 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT']) && (php_sapi_name() != 'cli')) {
 	$status_301 = 'HTTP/1.0 301 Moved Permanently';
 	$status_302 = 'HTTP/1.0 302 Moved Temporarily';
 
-	// Force non-www domain and HTTPS
-	if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.') {
+	if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
+		$site_domain = 'productionsite.com';
+	} else {
+		$site_domain = $_SERVER['HTTP_HOST'];
+	}
+
+	// Force HTTPS
+	if (empty($_SERVER['HTTPS'])) {
 		header($status_302);
-		header('Location: https://' . $_SERVER['HOST'] . $_SERVER['REQUEST_URI']);
+		header('Location: https://' . $site_domain . $_SERVER['REQUEST_URI'] . '/');
+		exit;
+	}
+
+	// Force non-www domain and HTTPS
+	if (substr($site_domain, 0, 4) === 'www.') {
+		$site_domain = str_replace('www.', '', $site_domain);
+		header($status_302);
+		header('Location: https://' . $site_domain . $_SERVER['REQUEST_URI']);
 		exit;
 	}
 
 	// Force www domain and HTTPS
-	// if (substr($_SERVER['HTTP_HOST'], 0, 4) !== 'www.') {
+	// if (substr($site_domain, 0, 4) !== 'www.') {
 	// 	header($status_302);
-	// 	header('Location: https://www.' . $_SERVER['HOST'] . $_SERVER['REQUEST_URI']);
+	// 	header('Location: https://www.' . $site_domain . $_SERVER['REQUEST_URI']);
 	// 	exit;
 	// }
 
 	// Match hostname with OR without www
-	if (preg_match('/(www\.)?carrotsense\.local/', $_SERVER['HTTP_HOST']) === 1) {
+	if (preg_match('/(www\.)?anotherdomain\.com/', $site_domain) === 1) {
 		header($status_302);
-		header('Location: ' . $site_protocol . $_SERVER['HOST'] . $_SERVER['REQUEST_URI']);
+		header('Location: ' . $site_protocol . $site_domain . $_SERVER['REQUEST_URI']);
 		exit();
 	}
 
@@ -36,61 +50,60 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT']) && (php_sapi_name() != 'cli')) {
 		'sub4.example.com',
 	])) {
 		header($status_302);
-		header('Location: ' . $site_protocol . $_SERVER['HOST'] . '/new-path-for-all/');
+		header('Location: ' . $site_protocol . $site_domain . '/new-path-for-all/');
 		exit();
 	}
 
 	// Match multiple paths
 	if (in_array($_SERVER['REQUEST_URI'], [
-		'/',
 		'/old',
 		'/another/path',
 		'/old-path',
 	])) {
 		header($status_302);
-		header('Location: ' . $site_protocol . $_SERVER['HOST'] . '/new-path-for-all/');
+		header('Location: ' . $site_protocol . $site_domain . '/new-path-for-all/');
 		exit();
 	}
 
 	// Match one path
 	if ($_SERVER['REQUEST_URI'] === '/old') {
 		header($status_302);
-		header('Location: ' . $site_protocol . $_SERVER['HOST'] . '/new-path/');
+		header('Location: ' . $site_protocol . $site_domain . '/new-path/');
 		exit();
 	}
 
 	// Match one path, trailing slash optional
 	if (preg_match('/\/requested-path(\/)?/', $_SERVER['REQUEST_URI']) === 1) {
 		header($status_302);
-		header('Location: ' . $site_protocol . $_SERVER['HOST'] . '/new-path/');
+		header('Location: ' . $site_protocol . $site_domain . '/new-path/');
 		exit();
 	}
 
 	// Match entire path directory
 	if (preg_match('/\/requested-path(.*)/', $_SERVER['REQUEST_URI']) === 1) {
 		header($status_302);
-		header('Location: ' . $site_protocol . $_SERVER['HOST'] . '/new-path/');
+		header('Location: ' . $site_protocol . $site_domain . '/new-path/');
 		exit();
 	}
 
 	// Match URL parameter specific value
 	if (isset($_GET['foo']) && $_GET['foo'] === 'bar') {
 		header($status_302);
-		header('Location: ' . $site_protocol . $_SERVER['HOST'] . '/new-path/');
+		header('Location: ' . $site_protocol . $site_domain . '/new-path/');
 		exit();
 	}
 
 	// Match URL parameter exists
 	if (isset($_GET['foo'])) {
 		header($status_302);
-		header('Location: ' . $site_protocol . $_SERVER['HOST'] . '/new-path/');
+		header('Location: ' . $site_protocol . $site_domain . '/new-path/');
 		exit();
 	}
 
 	// Match URL that has any parameter
 	if (!empty($_GET)) {
 		header($status_302);
-		header('Location: ' . $site_protocol . $_SERVER['HOST'] . '/new-path/');
+		header('Location: ' . $site_protocol . $site_domain . '/new-path/');
 		exit();
 	}
 
