7fede5023f3917c4e176d71507ea8ea460f1a3b3
Adam Walter <adam@vtldesign.com>
2019-02-21 11:30:13 -0500

    Adds Wistia support to Magnific Popup catch-all script

--- js/magnific-popup-catchall.js
+++ js/magnific-popup-catchall.js
@@ -1,86 +1,108 @@
 // Magnific Popup Catch-all
-
 var mpYouTubeOpts = {
-    type: 'iframe',
-    iframe: {
-        patterns: {
-            youtube: {
-                index: 'youtube.com/',
-                id: 'v=',
-                src: 'https://www.youtube.com/embed/%id%?autoplay=1&autohide=1&modestbranding=1&rel=0&showinfo=0'
-            },
-            youtube_short: {
-                index: 'youtu.be/',
-                id: 'youtu.be/',
-                src: 'https://www.youtube.com/embed/%id%?autoplay=1&autohide=1&modestbranding=1&rel=0&showinfo=0'
-            }
-        }
-    },
-    removalDelay: 300,
-    mainClass: 'mfp-fade'
+	type: 'iframe',
+	iframe: {
+		patterns: {
+			youtube: {
+				index: 'youtube.com/',
+				id: 'v=',
+				src: 'https://www.youtube.com/embed/%id%?autoplay=1&autohide=1&modestbranding=1&rel=0&showinfo=0'
+			},
+			youtube_short: {
+				index: 'youtu.be/',
+				id: 'youtu.be/',
+				src: 'https://www.youtube.com/embed/%id%?autoplay=1&autohide=1&modestbranding=1&rel=0&showinfo=0'
+			}
+		}
+	},
+	removalDelay: 300,
+	mainClass: 'mfp-fade'
 },
 mpVimeoOpts = {
-    type: 'iframe',
-    removalDelay: 300,
+	type: 'iframe',
+	removalDelay: 300,
+	mainClass: 'mfp-fade'
+},
+mpWistiaOpts = {
+	type: 'iframe',
+	iframe: {
+		patterns: {
+			wistia: {
+				index: 'wistia.com',
+				id: function(url) {
+					var m = url.match(/^.+wistia.com\/(medias)\/([^_]+)[^#]*(#medias=([^_&]+))?/);
+					if (m !== null) {
+						if(m[4] !== undefined) {
+							return m[4];
+						}
+						return m[2];
+					}
+					return null;
+				},
+				src: 'https://fast.wistia.net/embed/iframe/%id%?autoPlay=1&muted=0&endVideoBehavior=reset' // https://wistia.com/doc/embed-options#options_list
+			}
+		}
+	},
+	removalDelay: 300,
     mainClass: 'mfp-fade'
 },
 mpImageOpts = {
-    type: 'image',
-    closeOnContentClick: true,
-    fixedContentPos: true,
-    image: {
-        verticalFit: true,
-        titleSrc: function(item) {
-            var link    = item.el,
-                img     = link.find('img'),
-                title   = img.attr('title'),
-                alt     = img.attr('alt'),
-                caption = '';
-            if (typeof alt !== 'undefined') {
-                caption = alt;
-            } else if (typeof title !== 'undefined') {
-                caption = title;
-            }
-            return caption;
-        }
-    },
-    zoom: {
-        enabled: true,
-        duration: 300,
-        opener: function(element) {
-            return element.find('img');
-        }
-    }
+	type: 'image',
+	closeOnContentClick: true,
+	fixedContentPos: true,
+	image: {
+		verticalFit: true,
+		titleSrc: function(item) {
+			var link    = item.el,
+				img     = link.find('img'),
+				title   = img.attr('title'),
+				alt     = img.attr('alt'),
+				caption = '';
+			if (typeof alt !== 'undefined') {
+				caption = alt;
+			} else if (typeof title !== 'undefined') {
+				caption = title;
+			}
+			return caption;
+		}
+	},
+	zoom: {
+		enabled: true,
+		duration: 300,
+		opener: function(element) {
+			return element.find('img');
+		}
+	}
 },
 mpGalleryOpts = {
-    type: 'image',
-    delegate: 'a',
-    image: {
-        verticalFit: true,
-        titleSrc: function(item) {
-            var link    = item.el,
-                img     = link.find('img'),
-                title   = img.attr('title'),
-                alt     = img.attr('alt'),
-                caption = '';
-            if (typeof alt !== 'undefined') {
-                caption = alt;
-            } else if (typeof title !== 'undefined') {
-                caption = title;
-            }
-            return caption;
-        }
-    },
-    gallery: {
-        enabled: true
-    },
-    zoom: {
-        enabled: true,
-        duration: 300,
-        opener: function(element) {
-            return element.find('img');
-        }
-    }
+	type: 'image',
+	delegate: 'a',
+	image: {
+		verticalFit: true,
+		titleSrc: function(item) {
+			var link    = item.el,
+				img     = link.find('img'),
+				title   = img.attr('title'),
+				alt     = img.attr('alt'),
+				caption = '';
+			if (typeof alt !== 'undefined') {
+				caption = alt;
+			} else if (typeof title !== 'undefined') {
+				caption = title;
+			}
+			return caption;
+		}
+	},
+	gallery: {
+		enabled: true
+	},
+	zoom: {
+		enabled: true,
+		duration: 300,
+		opener: function(element) {
+			return element.find('img');
+		}
+	}
 };
 
 // Links with .lightbox class
@@ -96,3 +118,4 @@ $('.gallery').magnificPopup(mpGalleryOpts);
 $('a[href*="youtube.com"]').magnificPopup(mpYouTubeOpts);
 $('a[href*="youtu.be"]').magnificPopup(mpYouTubeOpts);
 $('a[href*="vimeo.com"]').magnificPopup(mpVimeoOpts);
+$('a[href*="wistia.com"]').magnificPopup(mpWistiaOpts);
